7ed53c5a757916dd6b20dddc16298e86128a87b2
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-21 14:40:50 +0800

    heroku friendly database.yml

--- config/db.rb
+++ config/db.rb
@@ -1,7 +1,10 @@
 require 'yaml'
+require 'erb'
 require 'sinatra/activerecord'
 
 ActiveRecord::Base.establish_connection(
-    YAML.load_file("#{settings.app_root}/config/database.yml")[settings.environment.to_s]
+    YAML.load(
+        ERB.new(File.read("#{settings.app_root}/config/database.yml")).result
+    )[settings.environment.to_s]
 )
 
