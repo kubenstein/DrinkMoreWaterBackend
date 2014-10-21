1d550f2b3fa84ba12d3daaa78818574200e82e0a
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-21 13:24:58 +0800

    add Bundle.require

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+require 'bundler'
+ENV['RACK_ENV'] ||= :development
+Bundler.require(:default, ENV['RACK_ENV'].to_sym)
+
 APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
 require 'sinatra'
 require './config/application'
