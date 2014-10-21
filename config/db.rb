b15faa98e0d486318bde9a1581b4ea8895bc5150
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-21 15:26:36 +0800

    AR logging

--- config/db.rb
+++ config/db.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'erb'
 require 'sinatra/activerecord'
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection(
     YAML.load(
         ERB.new(File.read("#{settings.app_root}/config/database.yml")).result
