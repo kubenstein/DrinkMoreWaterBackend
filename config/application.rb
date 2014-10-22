00a346b5170c8aea6983c5aed23c8d6cf7adb499
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-22 17:33:14 +0800

    load db only when needed

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,12 @@
 require 'json'
 require 'sinatra'
 require 'sinatra/namespace'
-require './config/db'
-require_all './app/models/*'
-require_all './app/workflows/*'
 require './config/environment_specific_config'
 require './app/app'
+require_all './app/models/*'
+require_all './app/workflows/*'
+autoload :DB, './config/db'
+
 
 set :app_root, APP_ROOT
 set :root, File.join(APP_ROOT, 'app')
