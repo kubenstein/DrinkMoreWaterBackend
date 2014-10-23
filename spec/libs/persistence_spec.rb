b42dc096588b30d2e256b57973603eb4ca00b65b
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-23 17:19:25 +0800

    Persistence module specs

---
+++ spec/libs/persistence_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../spec_helper'
+
+describe Persistence do
+  PERSISTABLE = Struct.new(:attribute1, :attribute2) do
+    extend Persistence
+  end
+
+  let(:fake_db) { [] }
+  let(:empty_class) { Class.new }
+  let(:persistable_class) { PERSISTABLE }
+
+
+  it 'should provide "all" method to extended class' do
+    expect(empty_class).not_to respond_to(:all)
+    empty_class.extend Persistence
+    expect(empty_class).to respond_to(:all)
+  end
+
+  it 'under "all" method, asks db to get all donations using model schema' do
+    fake_persistences_list = [
+        {attribute1: '1attribute1_value', attribute2: '1attribute2_value'},
+        {attribute1: '2attribute1_value', attribute2: '2attribute2_value'}
+    ]
+
+    ::DB = fake_db
+    dataset = double
+    expect(DB).to receive(:[]).with(:persistables).and_return(dataset)
+    expect(dataset).to receive(:select).with(:attribute1, :attribute2).and_return(fake_persistences_list)
+    results = persistable_class.all
+    expect(results).to have(2).items
+
+    expect(results[0]).to eq(PERSISTABLE.new('1attribute1_value', '1attribute2_value'))
+    expect(results[1]).to eq(PERSISTABLE.new('2attribute1_value', '2attribute2_value'))
+  end
+
+end
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,28 +1,13 @@
 require_relative '../spec_helper'
-#
-#
-# WHOLE THIS SHOULD BE TESTING LIB/PERSISTANCE NOT DONATION
-#
-#
-describe Donation do
-  subject { Donation }
-  let(:fake_db) { [] }
-  let(:fake_donation_list) do
-    [
-        {name: 'name1', timestamp: 1412897146},
-        {name: 'name2', timestamp: 1413897146},
-    ]
-  end
 
-  it 'under "all" method, asks db to get all donations' do
-    ::DB = fake_db
-    dataset = double()
-    expect(DB).to receive(:[]).with(:donations).and_return(dataset)
-    expect(dataset).to receive(:select).with(:name, :created_at).and_return(fake_donation_list)
-    results = subject.all
-    expect(results).to have(2).items
-    expect(results[0]).to eq(Donation.new('name1', 1412897146))
-    expect(results[1]).to eq(Donation.new('name2', 1413897146))
+describe Donation do
+
+  it 'should store business fields' do
+    subject.name = 'name'
+    subject.created_at = 'created_at'
+
+    expect(subject.name).to eq 'name'
+    expect(subject.created_at).to eq 'created_at'
   end
 
 end
