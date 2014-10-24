286e28049644c97a11124fa3c4b99cca0e58dc07
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-24 17:25:32 +0800

    donation validation

--- app/workflows/create_donation_workflow.rb
+++ app/workflows/create_donation_workflow.rb
@@ -1,19 +1,35 @@
 class CreateDonationWorkflow
-  attr_reader :model
+  attr_reader :model, :model_class
 
-  def call(donation_params)
-    params_values = permitted_params_values(donation_params, Donation.members)
-    @model = Donation.new(*params_values)
-    @model.create
+  def call(donation_params, model_class=Donation)
+    @model_class = model_class
+    @model = create_model(donation_params)
+
+    if valid?
+      @model.create
+    else
+      false
+    end
   end
 
 
   private
 
-  def permitted_params_values(params={}, permitted_keys)
+  def permitted_params_values(params)
+    params ||= {}
     ordered_permitted_values = []
+    permitted_keys = model_class.members
+
     permitted_keys.each { |permitted_key| ordered_permitted_values << params[permitted_key.to_s] }
     ordered_permitted_values.compact
+  end
+
+  def create_model(params)
+    model_class.new(*permitted_params_values(params))
+  end
+
+  def valid?
+    !model.name.empty?
   end
 
 end
--- spec/workflows/create_donation_workflow_spec.rb
+++ spec/workflows/create_donation_workflow_spec.rb
@@ -6,11 +6,18 @@ describe CreateDonationWorkflow do
   let(:good_params_values) { good_params.values }
   let(:bad_params) { {'name' => 'new_name', 'extra_param' => 'DONT_PASS_ME_TO_MODEL'} }
   let(:donation) { instance_double(Donation, name: 'new_donation').as_null_object }
+  let(:invalid_dotation) { instance_double(Donation, name: '').as_null_object }
 
   it 'saves model with given params' do
     expect(Donation).to receive(:new).with(*good_params_values).and_return(donation)
     expect(donation).to receive(:create)
     subject.call(good_params)
+  end
+
+  it 'doesnt save model without name' do
+    allow(Donation).to receive(:new).and_return(invalid_dotation)
+    expect(donation).not_to receive(:create)
+    subject.call({dummy: :dummy})
   end
 
   it 'returns model' do
