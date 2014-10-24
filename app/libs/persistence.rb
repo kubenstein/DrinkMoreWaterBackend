89d6611bf3b6c4d3b733484e0e2e8e60d104f4db
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-24 13:39:42 +0800

    add save to Persistence interface

--- app/libs/persistence.rb
+++ app/libs/persistence.rb
@@ -1,5 +1,21 @@
 module Persistence
-  def all
-    DB["#{self.name.downcase}s".to_sym].select(*self.members).map { |data| self.new(*data.values) }
+
+  module ClassMethods
+    def all
+      DB["#{self.name.downcase}s".to_sym].select(*self.members).map { |data| self.new(*data.values) }
+    end
+  end
+
+
+  module InstanceMethods
+    def save
+      true
+    end
+  end
+
+
+  def self.extended(base)
+    base.extend ClassMethods
+    base.include InstanceMethods
   end
 end
--- spec/workflows/create_donation_workflow_spec.rb
+++ spec/workflows/create_donation_workflow_spec.rb
@@ -6,19 +6,19 @@ describe CreateDonationWorkflow do
   let(:bad_params) { {'name' => 'new_name', 'extra_param' => 'DONT_PASS_ME_TO_MODEL'} }
   let(:donation) { instance_double(Donation, name: 'new_donation').as_null_object }
 
-  xit 'saves model with given params' do
+  it 'saves model with given params' do
     expect(Donation).to receive(:new).with(good_params).and_return(donation)
     expect(donation).to receive(:save)
     subject.call(good_params)
   end
 
-  xit 'returns model' do
+  it 'returns model' do
     expect(Donation).to receive(:new).and_return(donation)
     subject.call(good_params)
     expect(subject.model).to eq donation
   end
 
-  xit 'sanitize params for model' do
+  it 'sanitize params for model' do
     expect(Donation).to receive(:new).with(good_params).and_return(donation)
     subject.call(bad_params)
   end
