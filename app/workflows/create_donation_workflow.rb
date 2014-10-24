6b3b7299d34bd459965970fb2d17fb5df574c53b
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-24 15:59:29 +0800

    fix empty params donation creation

--- app/workflows/create_donation_workflow.rb
+++ app/workflows/create_donation_workflow.rb
@@ -2,16 +2,18 @@ class CreateDonationWorkflow
   attr_reader :model
 
   def call(donation_params)
-    params = permitted_params(donation_params, Donation.members)
-    @model = Donation.new(params)
+    params_values = permitted_params_values(donation_params, Donation.members)
+    @model = Donation.new(*params_values)
     @model.create
   end
 
 
   private
 
-  def permitted_params(params, permitted_keys)
-    params.keep_if { |key, value| permitted_keys.include?(key.to_sym) }
+  def permitted_params_values(params={}, permitted_keys)
+    ordered_permitted_values = []
+    permitted_keys.each { |permitted_key| ordered_permitted_values << params[permitted_key.to_s] }
+    ordered_permitted_values.compact
   end
 
 end
