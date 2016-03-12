4e87c4aabd76d1462c878e414b989e894a1916cf
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-03-12 14:31:45 +0700

    remove extra route

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,6 +1,3 @@
-post '/donations', to: 'donations#create'
-# Configure your routes here
-# See: http://www.rubydoc.info/gems/hanami-router/#Usage
 namespace '/v1' do
   get '/donations', to: 'donations#index'
   post '/donations', to: 'donations#create'
